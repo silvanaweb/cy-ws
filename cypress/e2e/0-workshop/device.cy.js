f9a9227c41ce42ab455b629f608fdb21bb51874a
Silvana Donato <silvana.donato@vodafone.com>
2023-11-29 11:54:39 +0000

    create tests outline

---
+++ cypress/e2e/0-workshop/device.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+context('PAYG', () => {
+  beforeEach(() => {
+    cy.visit('https://www.vodafone.co.uk/mobile/phones/pay-as-you-go/google/pixel-7')
+    // cy.visit('https://www.vodafone.co.uk/mobile/phones/pay-as-you-go/google/pixel-7a')
+    // cy.visit('https://www.vodafone.co.uk/mobile/phones/pay-as-you-go/google/pixel-8')
+  })
+
+  it('select colour', () => {
+
+    // select colour and assert that has been selected
+
+  })
+
+  it('shows plans', () => {
+    // assert that plans are showing and how many of them
+  })
+
+})
---
+++ cypress/e2e/0-workshop/devicelist.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+context('PAYG', () => {
+  beforeEach(() => {
+    cy.visit('https://www.vodafone.co.uk/mobile/phones/pay-as-you-go')
+  })
+
+  it('filter by samsung logo', () => {
+
+  })
+
+  it('got to the details page of the first device', () => {
+
+    // assert that the title is loading
+  })
+
+})
