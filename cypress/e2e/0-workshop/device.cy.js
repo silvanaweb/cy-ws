c134a7e03b4a275e74f5db567120286796e0cf2a
Silvana Donato <silvana.donato@vodafone.com>
2023-11-29 12:27:17 +0000

    suggested solutions

--- cypress/e2e/0-workshop/device.cy.js
+++ cypress/e2e/0-workshop/device.cy.js
@@ -5,16 +5,24 @@ context('PAYG', () => {
     cy.visit('https://www.vodafone.co.uk/mobile/phones/pay-as-you-go/google/pixel-7')
     // cy.visit('https://www.vodafone.co.uk/mobile/phones/pay-as-you-go/google/pixel-7a')
     // cy.visit('https://www.vodafone.co.uk/mobile/phones/pay-as-you-go/google/pixel-8')
+    cy.get('#onetrust-accept-btn-handler').then(($el) => {
+      if ($el.length) {
+      // Element exists, do something
+        $el.trigger('click')
+      }
+      });
   })
 
   it('select colour', () => {
-
+    cy.contains('Obsidian').click()
     // select colour and assert that has been selected
-
+    cy.get('input[name="device-variant-color"]').check('obsidian')
   })
 
   it('shows plans', () => {
+    cy.contains('Continue to plans').click()
     // assert that plans are showing and how many of them
+    cy.get('[data-cy="planCard"]').should('have.length', 5)
   })
 
 })
--- cypress/e2e/0-workshop/devicelist.cy.js
+++ cypress/e2e/0-workshop/devicelist.cy.js
@@ -3,15 +3,23 @@
 context('PAYG', () => {
   beforeEach(() => {
     cy.visit('https://www.vodafone.co.uk/mobile/phones/pay-as-you-go')
+    cy.get('#onetrust-accept-btn-handler').then(($el) => {
+      if ($el.length) {
+      // Element exists, do something
+        $el.trigger('click')
+      }
+      });
   })
 
   it('filter by samsung logo', () => {
-
+    cy.get('[data-cy="samsungLogoItem"]').click()
+    cy.get('[data-component-name="ContainerWithLabel"]').should('have.length', 18)
   })
 
   it('got to the details page of the first device', () => {
-
+    cy.contains('See details').first().click()
     // assert that the title is loading
+    cy.get('[data-component-name="Heading"]').should('contain', '8100')
   })
 
 })
